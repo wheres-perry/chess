2e01c9da82b7f1709c28872a00b545ab0b92620c
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-01-23 22:07:13 +0000

    Added knight functionality

--- shared/src/main/java/chess/Knight.java
+++ shared/src/main/java/chess/Knight.java
@@ -1,17 +1,39 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Knight extends ChessMovesCalculator {
 
-    public Knight(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type, boolean HasMoved,
-            boolean EPable, ChessBoard board, ChessPosition position) {
-        super(pieceColor, type, HasMoved, EPable, board, position);
+    public Knight(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
+        super(color, ChessPiece.PieceType.QUEEN, board, position);
     }
 
     @Override
     final public Collection<ChessMove> moves() {
-        // TODO Implement Knight Moves
-        throw new RuntimeException("Stub");
+        final List<int[]> KNIGHT_COORDS = List.of(
+                new int[] { 1, 2 },
+                new int[] { 2, 1 },
+                new int[] { -1, 2 },
+                new int[] { -2, 1 },
+                new int[] { 1, -2 },
+                new int[] { 2, -1 },
+                new int[] { -1, -2 },
+                new int[] { -2, -1 });
+
+        Collection<ChessMove> output = new ArrayList<>();
+        for (int[] coord : KNIGHT_COORDS) {
+            ChessPosition target = new ChessPosition(position.getRow() + coord[0], position.getColumn() + coord[1]);
+
+            if (!target.InBounds()) {
+                continue;
+            }
+            if (IsFriend(board, target)) {
+                continue;
+            }
+            output.add(new ChessMove(position, target));
+        }
+        return output;
     }
 }
