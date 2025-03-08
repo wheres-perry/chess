af6da2d9ad07e8489e62360621c1b4bd49e5a0ac
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-03-08 04:00:01 +0000

    Switched to correct properties file setup

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -11,20 +11,7 @@ import java.util.Properties;
 
 public class MySQLAuthDAO implements AuthDAO {
 
-    private static final String DB_URL = "placeholder";
-    private static final String DB_USER = "placeholder";
-    private static final String DB_PASSWORD = "placeholder";
-
     public MySQLAuthDAO() throws DataAccessException {
-        Properties props = new Properties();
-        try (FileInputStream fis = new FileInputStream("database.properties")) {
-            props.load(fis);
-            String DB_URL = props.getProperty("db.username");
-            String DB_USER = props.getProperty("db.username");
-            String DB_PASSWORD = props.getProperty("db.password");
-        } catch (IOException e) {
-            // Uh OH!
-        }
     }
 
     @Override
