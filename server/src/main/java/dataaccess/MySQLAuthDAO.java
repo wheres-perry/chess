9a4ab671f9c2eb06d8aa326159969ad2ec6c8c59
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-03-08 03:27:11 +0000

    Added properties file to store secrets

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -4,6 +4,11 @@ import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import model.AuthData;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class MySQLAuthDAO implements AuthDAO {
 
     private static final String DB_URL = "placeholder";
@@ -11,6 +16,15 @@ public class MySQLAuthDAO implements AuthDAO {
     private static final String DB_PASSWORD = "placeholder";
 
     public MySQLAuthDAO() throws DataAccessException {
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream("database.properties")) {
+            props.load(fis);
+            String DB_URL = props.getProperty("db.username");
+            String DB_USER = props.getProperty("db.username");
+            String DB_PASSWORD = props.getProperty("db.password");
+        } catch (IOException e) {
+            // Uh OH!
+        }
     }
 
     @Override
