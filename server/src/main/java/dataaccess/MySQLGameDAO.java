c05e3a7e89207d9d5986f824748e61dcfe9cec88
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-03-09 03:41:44 +0000

    Added skeleton for MySQLGameDAO.java

---
+++ server/src/main/java/dataaccess/MySQLGameDAO.java
@@ -0,0 +1,78 @@
+package dataaccess;
+
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import dataaccess.GameDAO;
+import java.util.Collection;
+import java.util.ArrayList;
+import model.GameData;
+import chess.ChessGame;
+
+/**
+ * MySQL implementation of the GameDAO interface.
+ */
+public class MySQLGameDAO implements GameDAO {
+
+  /**
+   * Creates the games table if it does not already exist.
+   * 
+   * @throws DataAccessException if there is an error creating the table
+   */
+  private void createTableIfNotExists() throws DataAccessException {
+  }
+
+  public MySQLGameDAO() throws DataAccessException {
+  }
+
+  /**
+   * Creates a new game.
+   * 
+   * @param gameName the name of the game
+   * @return the ID of the newly created game
+   * @throws DataAccessException if there is an error creating the game
+   */
+  @Override
+  public int createGame(String gameName) throws DataAccessException {
+  }
+
+  /**
+   * Gets a game by ID.
+   * 
+   * @param gameID the game ID to look up
+   * @return the game data
+   * @throws DataAccessException if there is an error retrieving the game
+   */
+  @Override
+  public GameData getGame(int gameID) throws DataAccessException {
+  }
+
+  /**
+   * Lists all games.
+   * 
+   * @return a list of all games
+   * @throws DataAccessException if there is an error listing the games
+   */
+  @Override
+  public Collection<GameData> listGames() throws DataAccessException {
+  }
+
+  /**
+   * Updates a game's information.
+   * 
+   * @param gameID the ID of the game to update
+   * @param game   the updated game data
+   * @throws DataAccessException if there is an error updating the game
+   */
+  @Override
+  public void updateGame(int gameID, GameData game) throws DataAccessException {
+  }
+
+  /**
+   * Clears all games.
+   * 
+   * @throws DataAccessException if there is an error clearing the games
+   */
+  @Override
+  public void clear() throws DataAccessException {
+  }
+}
