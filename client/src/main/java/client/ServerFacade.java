3089a7d3fc7c5432300801f8a47d2bec9012e03d
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-03-15 12:13:29 -0600

    Added new classes for server facade and login REPLs; updated EscapeSequences documentation

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,5 +1,46 @@
 package client;
 
+import java.util.Map;
+
 public class ServerFacade {
-    
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public void clear() throws Exception {
+        // Stub implementation
+    }
+
+    // Map<String, Object> is a can be used to store key-value pairs, helpful for JSON responses
+    // object keeps it generic so it can be nested or any obejct.
+    public Map<String, Object> register(String username, String password, String email) throws Exception {
+        // Stub implementation
+        return null;
+    }
+
+    public Map<String, Object> login(String username, String password) throws Exception {
+        // Stub implementation
+        return null;
+    }
+
+    public void logout(String authToken) throws Exception {
+        // Stub implementation
+    }
+
+    public Map<String, Object> listGames(String authToken) throws Exception {
+        // Stub implementation
+        return null;
+    }
+
+    public Map<String, Object> createGame(String authToken, String gameName) throws Exception {
+        // Stub implementation
+        return null;
+    }
+
+    public Map<String, Object> joinGame(String authToken, int gameID, String playerColor) throws Exception {
+        // Stub implementation
+        return null;
+    }
 }
