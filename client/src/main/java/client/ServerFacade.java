a59984004b29a5392a78ca8949e5f11af88d55ca
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-03-17 15:24:51 -0600

    Add doc strings and set up function signatures

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -2,42 +2,102 @@ package client;
 
 import java.util.Map;
 
+/**
+ * Facade for the Chess server API. Handles all server communication.
+ */
 public class ServerFacade {
     private final String serverUrl;
 
+    /**
+     * Creates a new ServerFacade.
+     * 
+     * @param url The URL of the server
+     */
     public ServerFacade(String url) {
         serverUrl = url;
     }
 
+    /**
+     * Clears the database.
+     * 
+     * @throws Exception if an error occurs during the operation
+     */
     public void clear() throws Exception {
         // Stub implementation
     }
 
+    /**
+     * Registers a new user.
+     * 
+     * @param username The username
+     * @param password The password
+     * @param email The email address
+     * @return Response data from the server
+     * @throws Exception if an error occurs during the operation
+     */
     public Map<String, Object> register(String username, String password, String email)
             throws Exception {
         // Stub implementation
         return null;
     }
 
+    /**
+     * Logs in a user.
+     * 
+     * @param username The username
+     * @param password The password
+     * @return Response data from the server
+     * @throws Exception if an error occurs during the operation
+     */
     public Map<String, Object> login(String username, String password) throws Exception {
         // Stub implementation
         return null;
     }
 
+    /**
+     * Logs out a user.
+     * 
+     * @param authToken The authentication token
+     * @throws Exception if an error occurs during the operation
+     */
     public void logout(String authToken) throws Exception {
         // Stub implementation
     }
 
+    /**
+     * Lists all games.
+     * 
+     * @param authToken The authentication token
+     * @return Response data from the server
+     * @throws Exception if an error occurs during the operation
+     */
     public Map<String, Object> listGames(String authToken) throws Exception {
         // Stub implementation
         return null;
     }
 
+    /**
+     * Creates a new game.
+     * 
+     * @param authToken The authentication token
+     * @param gameName The name of the game
+     * @return Response data from the server
+     * @throws Exception if an error occurs during the operation
+     */
     public Map<String, Object> createGame(String authToken, String gameName) throws Exception {
         // Stub implementation
         return null;
     }
 
+    /**
+     * Joins an existing game.
+     * 
+     * @param authToken The authentication token
+     * @param gameID The ID of the game to join
+     * @param playerColor The color the player wants to play as (WHITE/BLACK), or null to observe
+     * @return Response data from the server
+     * @throws Exception if an error occurs during the operation
+     */
     public Map<String, Object> joinGame(String authToken, int gameID, String playerColor)
             throws Exception {
         // Stub implementation
