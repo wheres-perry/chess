0bdcc47eb9d38246428833fd55650e02c0f66fd2
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-03-23 13:13:13 -0600

    Fixed formatting and formatter

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -1,7 +1,8 @@
 package ui;
 
 /**
- * This class contains constants and functions relating to ANSI Escape Sequences that are useful in
+ * This class contains constants and functions relating to ANSI Escape Sequences
+ * that are useful in
  * the Client display
  */
 public class EscapeSequences {
