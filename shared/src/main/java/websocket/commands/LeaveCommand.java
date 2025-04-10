eab1e7a9caaf4164e8302543fd4acf8ee645eae9
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-04-10 21:42:12 +0000

    Implement LeaveCommand and ResignCommand classes to extend UserGameCommand with appropriate constructors

--- shared/src/main/java/websocket/commands/LeaveCommand.java
+++ shared/src/main/java/websocket/commands/LeaveCommand.java
@@ -1,5 +1,7 @@
 package websocket.commands;
 
-public class LeaveCommand {
-
+public class LeaveCommand extends UserGameCommand {
+  public LeaveCommand(String authToken, Integer gameID) {
+    super(CommandType.LEAVE, authToken, gameID);
+  }
 }
--- shared/src/main/java/websocket/commands/ResignCommand.java
+++ shared/src/main/java/websocket/commands/ResignCommand.java
@@ -1,5 +1,7 @@
 package websocket.commands;
 
-public class ResignCommand {
-
+public class ResignCommand extends UserGameCommand {
+  public ResignCommand(String authToken, Integer gameID) {
+    super(CommandType.RESIGN, authToken, gameID);
+  }
 }
