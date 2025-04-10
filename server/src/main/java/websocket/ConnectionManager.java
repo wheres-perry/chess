430c598a52544e49c477cf35205fc49daa8cecc9
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-04-10 21:50:04 +0000

    Refactor ConnectionManager to use ConcurrentHashMap for game connections and remove unused methods

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -1,39 +1,39 @@
 package websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
-import webSocketMessages.Notification;
-
+import websocket.messages.ServerMessage;
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ConnectionManager {
-  public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+  public final ConcurrentHashMap<Integer, ConcurrentHashMap<String, Connection>> gameConnections = new ConcurrentHashMap<>();
 
   public void add(String visitorName, Session session) {
-    var connection = new Connection(visitorName, session);
-    connections.put(visitorName, connection);
+    throw new RuntimeException("Stub error throws");
   }
 
-  public void remove(String visitorName) {
-    connections.remove(visitorName);
+  public void add(String username, Session session, Integer gameID) {
+    throw new RuntimeException("Stub error throws");
   }
 
-  public void broadcast(String excludeVisitorName, Notification notification) throws IOException {
-    var removeList = new ArrayList<Connection>();
-    for (var c : connections.values()) {
-      if (c.session.isOpen()) {
-        if (!c.visitorName.equals(excludeVisitorName)) {
-          c.send(notification.toString());
-        }
-      } else {
-        removeList.add(c);
-      }
-    }
+  public void remove(String username, Integer gameID) {
+    throw new RuntimeException("Stub error throws");
+  }
 
-    // Clean up any connections that were left open.
-    for (var c : removeList) {
-      connections.remove(c.visitorName);
-    }
+  public void sendToOne(String username, String message) throws IOException {
+    throw new RuntimeException("Stub error throws");
+  }
+
+  public void broadcast(String excludeUsername, Integer gameID, String message) throws IOException {
+    throw new RuntimeException("Stub error throws");
+  }
+
+  private Connection findConnection(String username) {
+    throw new RuntimeException("Stub error throws");
   }
 }
