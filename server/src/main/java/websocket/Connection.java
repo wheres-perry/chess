c8817b811f3e82d8f1d9bfc58d09ae7e10611c4c
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2025-04-14 20:04:43 +0000

    Refactor Connection class to include game ID and enhance message sending logic

--- server/src/main/java/websocket/Connection.java
+++ server/src/main/java/websocket/Connection.java
@@ -3,17 +3,55 @@ package websocket;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
+import java.util.Objects; // Keep for equals/hashCode
 
+/**
+ * Represents a single WebSocket connection associated with a user and a game.
+ */
 public class Connection {
-  public String visitorName;
-  public Session session;
+  public final String visitorName; // Typically the username
+  public final Session session;
+  public final Integer gameID; // Added game ID
 
-  public Connection(String visitorName, Session session) {
+  /**
+   * Constructs a new Connection.
+   * 
+   * @param gameID      The ID of the game this connection is associated with.
+   * @param visitorName The identifier for the user (e.g., username).
+   * @param session     The WebSocket session object.
+   */
+  public Connection(Integer gameID, String visitorName, Session session) {
+    this.gameID = gameID;
     this.visitorName = visitorName;
     this.session = session;
   }
 
+  /**
+   * Sends a text message over the WebSocket connection.
+   * 
+   * @param msg The message to send (expected to be JSON string).
+   * @throws IOException If sending fails.
+   */
   public void send(String msg) throws IOException {
-    session.getRemote().sendString(msg);
+    if (session.isOpen()) {
+      session.getRemote().sendString(msg);
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Connection that = (Connection) o;
+    // Primarily identify connection by session, but include others for completeness
+    return Objects.equals(session, that.session) && Objects.equals(visitorName, that.visitorName)
+        && Objects.equals(gameID, that.gameID);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(visitorName, session, gameID);
   }
 }
